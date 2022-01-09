d9cb92d35b9970616adb008ee9b4cddae73e8d2b
Luther Azi <luther.pnk@gmail.com>
2022-01-09 15:45:46 -0300

    Split Strings Kata Solution

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# kata-codewars
+# Kata Solutions - Code Wars
+
 My solutions for katas in Code Wars platform.
+
+http://www.codewars.com/
---
+++ split_strings.py
@@ -0,0 +1,18 @@
+"""
+Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+"""
+
+def solution(s):
+    list_pairs = []
+    pair = ''
+    
+    for value in s:
+        pair += value
+        if len(pair) == 2:
+            list_pairs.append(pair)
+            pair = ''
+    if pair:
+        list_pairs.append(pair + '_')
+    
+    return list_pairs
